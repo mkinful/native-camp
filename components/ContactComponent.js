34c21160f99ac3b430cff07f6e24638293bed148
Your Name <epyon85@gmail.com>
2021-01-15 19:43:50 -0500

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, Linking } from 'react-native';
 import {Card} from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
     constructor(props) {
@@ -14,13 +15,15 @@ class Contact extends Component {
     render(){
         return(
             <ScrollView>
-                <Card title = "Contact Information" wrapperStyle={{margin: 20}}>
-                <Text> 1 Nucamp Way </Text>
-                <Text>Seattle, WA 98001</Text>
-                <Text style={{marginBottom:10}}>U.S.A</Text>
-                <Text onPress={()=> Linking.openURL(`tel:1-206-555-1234`)}>Phone: 1-206-555-1234</Text>
-                <Text onPress={()=> Linking.openURL('mailto:campsites@nucamp.co')} title="Email: campsites@nucamp.com">campsites@nucamp.com</Text>
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card title = "Contact Information" wrapperStyle={{margin: 20}}>
+                    <Text> 1 Nucamp Way </Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={{marginBottom:10}}>U.S.A</Text>
+                    <Text onPress={()=> Linking.openURL(`tel:1-206-555-1234`)}>Phone: 1-206-555-1234</Text>
+                    <Text onPress={()=> Linking.openURL('mailto:campsites@nucamp.co')} title="Email: campsites@nucamp.com">campsites@nucamp.com</Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         )
     }
